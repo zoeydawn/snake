69f48407b3eebc11fc705fe8ecfe50864966ce58
Zoey <z@zoey.ninja>
2023-02-08 16:40:37 -0500

    upated formatting, no more simicolons!

--- src/main.ts
+++ src/main.ts
@@ -1,81 +1,79 @@
-import { Point, Snake } from "./snake";
-import * as w4 from "./wasm4";
-
-// TODO: update the formatting and get rid of these damn simicolins
+import { Point, Snake } from './snake'
+import * as w4 from './wasm4'
 
 function rnd(n: i32 = 20): u16 {
-  return u16(Math.floor(Math.random() * n));
+  return u16(Math.floor(Math.random() * n))
 }
 
-const snake = new Snake();
-let fruit = new Point(rnd(20), rnd(20));
-let prevState: u8;
-let frameCount = 0;
+const snake = new Snake()
+let fruit = new Point(rnd(20), rnd(20))
+let prevState: u8
+let frameCount = 0
 const fruitSprite = memory.data<u8>([
   0x00, 0xa0, 0x02, 0x00, 0x0e, 0xf0, 0x36, 0x5c, 0xd6, 0x57, 0xd5, 0x57, 0x35,
   0x5c, 0x0f, 0xf0,
-]);
+])
 
 // Move snake based on user input
 function input(): void {
-  const gamepad = load<u8>(w4.GAMEPAD1);
-  const justPressed = gamepad & (gamepad ^ prevState);
+  const gamepad = load<u8>(w4.GAMEPAD1)
+  const justPressed = gamepad & (gamepad ^ prevState)
 
   if (justPressed & w4.BUTTON_LEFT) {
-    snake.left();
+    snake.left()
   }
   if (justPressed & w4.BUTTON_RIGHT) {
-    snake.right();
+    snake.right()
   }
   if (justPressed & w4.BUTTON_UP) {
-    snake.up();
+    snake.up()
   }
   if (justPressed & w4.BUTTON_DOWN) {
-    snake.down();
+    snake.down()
   }
 
-  prevState = gamepad;
+  prevState = gamepad
 }
 
 export function update(): void {
-  frameCount++;
+  frameCount++
 
-  input();
+  input()
 
   // speed control
   if (frameCount % 15 === 0) {
-    snake.update();
+    snake.update()
 
     if (snake.isDead()) {
       // TODO: display "Game Over", the score, and a "play again" button
-      snake.reset();
-      w4.trace("dead!");
+      snake.reset()
+      w4.trace('dead!')
     }
 
     // detect collision with fruit
     if (snake.body[0].equals(fruit)) {
-      let tail = snake.body[snake.body.length - 1];
+      let tail = snake.body[snake.body.length - 1]
 
       // grow the snake
-      snake.body.push(new Point(tail.x, tail.y));
+      snake.body.push(new Point(tail.x, tail.y))
 
       // relocate the fruit
       // TODO: don't allow the fruit to spawn inside the snake
-      fruit.x = rnd(20);
-      fruit.y = rnd(20);
+      fruit.x = rnd(20)
+      fruit.y = rnd(20)
     }
   }
-  snake.draw();
+  snake.draw()
 
   // because we set the drawing colors, we need to change the drawing colors to:
-  store<u16>(w4.DRAW_COLORS, 0x4320);
+  store<u16>(w4.DRAW_COLORS, 0x4320)
   // Blit draws a sprite at position `x`, `y` and uses DRAW_COLORS accordingly
-  w4.blit(fruitSprite, fruit.x * 8, fruit.y * 8, 8, 8, w4.BLIT_2BPP);
+  w4.blit(fruitSprite, fruit.x * 8, fruit.y * 8, 8, 8, w4.BLIT_2BPP)
 }
 
 export function start(): void {
-  store<u32>(w4.PALETTE, 0xfbf7f3, 0 * sizeof<u32>());
-  store<u32>(w4.PALETTE, 0xe5b083, 1 * sizeof<u32>());
-  store<u32>(w4.PALETTE, 0x426e5d, 2 * sizeof<u32>());
-  store<u32>(w4.PALETTE, 0x20283d, 3 * sizeof<u32>());
+  store<u32>(w4.PALETTE, 0xfbf7f3, 0 * sizeof<u32>())
+  store<u32>(w4.PALETTE, 0xe5b083, 1 * sizeof<u32>())
+  store<u32>(w4.PALETTE, 0x426e5d, 2 * sizeof<u32>())
+  store<u32>(w4.PALETTE, 0x20283d, 3 * sizeof<u32>())
 }
--- src/snake.ts
+++ src/snake.ts
@@ -1,68 +1,68 @@
-import * as w4 from "./wasm4";
+import * as w4 from './wasm4'
 
 export class Point {
   constructor(public x: i32, public y: i32) {}
 
   equals(other: Point): bool {
-    return this.x == other.x && this.y == other.y;
+    return this.x == other.x && this.y == other.y
   }
 }
 
 export class Snake {
-  body: Array<Point> = [new Point(2, 0), new Point(1, 0), new Point(0, 0)];
-  direction: Point = new Point(1, 0);
+  body: Array<Point> = [new Point(2, 0), new Point(1, 0), new Point(0, 0)]
+  direction: Point = new Point(1, 0)
 
   draw(): void {
-    store<u16>(w4.DRAW_COLORS, 0x0043);
-    this.body.forEach((part) => w4.rect(part.x * 8, part.y * 8, 8, 8));
+    store<u16>(w4.DRAW_COLORS, 0x0043)
+    this.body.forEach((part) => w4.rect(part.x * 8, part.y * 8, 8, 8))
 
-    store<u16>(w4.DRAW_COLORS, 0x0004);
-    w4.rect(this.body[0].x * 8, this.body[0].y * 8, 8, 8);
+    store<u16>(w4.DRAW_COLORS, 0x0004)
+    w4.rect(this.body[0].x * 8, this.body[0].y * 8, 8, 8)
   }
 
   update(): void {
-    const body = this.body;
+    const body = this.body
     // draw the snake
     for (let i = body.length - 1; i > 0; i--) {
-      unchecked((body[i].x = body[i - 1].x));
-      unchecked((body[i].y = body[i - 1].y));
+      unchecked((body[i].x = body[i - 1].x))
+      unchecked((body[i].y = body[i - 1].y))
     }
 
     // head movement
-    body[0].x = (body[0].x + this.direction.x) % 20;
-    body[0].y = (body[0].y + this.direction.y) % 20;
+    body[0].x = (body[0].x + this.direction.x) % 20
+    body[0].y = (body[0].y + this.direction.y) % 20
 
     if (body[0].x < 0) {
-      body[0].x = 19;
+      body[0].x = 19
     }
     if (body[0].y < 0) {
-      body[0].y = 19;
+      body[0].y = 19
     }
   }
 
   isDead(): bool {
-    const head = this.body[0];
+    const head = this.body[0]
 
     for (let i = 1, len = this.body.length; i < len; i++) {
       if (this.body[i].equals(head)) {
-        return true;
+        return true
       }
     }
 
-    return false;
+    return false
   }
 
   reset(): void {
-    this.body = [new Point(2, 0), new Point(1, 0), new Point(0, 0)];
-    this.direction = new Point(1, 0);
+    this.body = [new Point(2, 0), new Point(1, 0), new Point(0, 0)]
+    this.direction = new Point(1, 0)
   }
 
   left(): void {
     // w4.trace("left");
 
     if (this.direction.x == 0) {
-      this.direction.x = -1;
-      this.direction.y = 0;
+      this.direction.x = -1
+      this.direction.y = 0
     }
   }
 
@@ -70,8 +70,8 @@ export class Snake {
     // w4.trace("right");
 
     if (this.direction.x == 0) {
-      this.direction.x = 1;
-      this.direction.y = 0;
+      this.direction.x = 1
+      this.direction.y = 0
     }
   }
 
@@ -79,8 +79,8 @@ export class Snake {
     // w4.trace("up");
 
     if (this.direction.y == 0) {
-      this.direction.x = 0;
-      this.direction.y = -1;
+      this.direction.x = 0
+      this.direction.y = -1
     }
   }
 
@@ -88,8 +88,8 @@ export class Snake {
     // w4.trace("down");
 
     if (this.direction.y == 0) {
-      this.direction.x = 0;
-      this.direction.y = 1;
+      this.direction.x = 0
+      this.direction.y = 1
     }
   }
 }
