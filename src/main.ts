5daedcb7e75295339ea6fb97c6ba0458b0791ae8
Zoey <z@zoey.ninja>
2023-02-08 16:22:36 -0500

    cleaning up

--- src/main.ts
+++ src/main.ts
@@ -16,11 +16,6 @@ const fruitSprite = memory.data<u8>([
   0x5c, 0x0f, 0xf0,
 ]);
 
-// const smiley = memory.data<u8>([
-//   0b11000011, 0b10000001, 0b00100100, 0b00100100, 0b00000000, 0b00100100,
-//   0b10011001, 0b11000011,
-// ]);
-
 // Move snake based on user input
 function input(): void {
   const gamepad = load<u8>(w4.GAMEPAD1);
@@ -84,9 +79,3 @@ export function start(): void {
   store<u32>(w4.PALETTE, 0x426e5d, 2 * sizeof<u32>());
   store<u32>(w4.PALETTE, 0x20283d, 3 * sizeof<u32>());
 }
-
-// fruit
-// const fruitWidth = 8;
-// const fruitHeight = 8;
-// const fruitFlags = 1; // BLIT_2BPP
-// const fruitSprite = memory.data<u8>([ 0x00,0xa0,0x02,0x00,0x0e,0xf0,0x36,0x5c,0xd6,0x57,0xd5,0x57,0x35,0x5c,0x0f,0xf0 ]);
